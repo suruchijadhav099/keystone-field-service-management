fe0cae91abe4afb897b569c7882b59642fa4c226
suruchijadhav099 <s.suruchijadhav@gmail.com>
2026-09-19 13:04:03 +0530

    Fix production login API

--- src/main/java/controller/SpaController.java
+++ src/main/java/controller/SpaController.java
@@ -1,4 +1,13 @@
-package controller;
+package com.zidio.keystone.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SpaController {
+
+    @GetMapping({"/", "/login"})
+    public String forwardToIndex() {
+        return "forward:/index.html";
+    }
 }
